098f4338711b9629362a1810705c85fa2aa3f9a7
Dev Doge <96713574+devdoge999@users.noreply.github.com>
2022-01-01 01:59:36 +0900

    change compiler and linter

---
+++ contracts/RiraUniversityTomoz.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.6;
+
+import "hardhat/console.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Full.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Mintable.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Burnable.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Pausable.sol";
